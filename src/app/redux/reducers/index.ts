fb330a8992a40509d755a117f53242f0fb00f1ff
Umesh KS <umesh.kdev221@gmail.com>
2024-07-18 18:12:48 +0530

    Changes

--- src/app/redux/reducers/index.ts
+++ src/app/redux/reducers/index.ts
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
-import { authReducer as auth } from "./user";
+import { authReducer as auth } from "./auth";
 import { themeReducer as theme } from "./theme";
+import { usersReducer as users } from "./user";
 
 const tobePersisted = {
-   auth,
-   theme,
+  auth,
+  theme,
+  users,
 };
 
 export const reducers = combineReducers({
-   ...tobePersisted,
+  ...tobePersisted,
 });
 
 export const tobePersistedKeys = Object.keys(tobePersisted);
